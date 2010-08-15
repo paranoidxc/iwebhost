f1465bdf73d3fed54709aeb8f8cd7125a774ee50
Paranoid.xc <paranoid.xc@gmail.com>
2010-08-15 19:18:10 +0800

    shit

--- protected/modules/admin/views/article/_form.php
+++ protected/modules/admin/views/article/_form.php
@@ -7,46 +7,58 @@
 		'class' => 'ajax_form'
 	)
 )); ?>
+	<!-- <p class="note">Fields with <span class="required">*</span> are required.</p> -->
+	<?php //echo $form->errorSummary($model); ?>
+	<table class='itable'>
+	
+	<tr>
+		<th class='alt leftborder'><?php echo $form->labelEx($model,'title'); ?></th>
+		<td>
+			<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>100)); ?>
+			<?php echo $form->error($model,'title'); ?>
+		</td>
+	</tr>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<tr>
+		<th class='alt leftborder'><?php echo $form->labelEx($model,'subtitle'); ?></th>
+		<td>
+			<?php echo $form->textField($model,'subtitle',array('size'=>60,'maxlength'=>255)); ?>
+			<?php echo $form->error($model,'subtitle'); ?>
+		</td>
+	</tr>
 
-	<?php echo $form->errorSummary($model); ?>
+	<tr>
+		<th class='alt leftborder'><?php echo $form->labelEx($model,'desc'); ?></th>
+		<td>
+			<?php echo $form->textArea($model,'desc',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'desc'); ?>
+		</td>
+	</tr>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'title'); ?>
-		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>100)); ?>
-		<?php echo $form->error($model,'title'); ?>
-	</div>
+	<tr>
+		<th class='alt leftborder'><?php echo $form->labelEx($model,'content'); ?></th>
+		<td>
+			<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'content'); ?>
+		</td>
+	</tr>	
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'subtitle'); ?>
-		<?php echo $form->textField($model,'subtitle',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'subtitle'); ?>
-	</div>
+	<tr>
+		<th class='alt leftborder'><?php echo $form->labelEx($model,'category_id'); ?></th>
+		<td>
+			<?php echo $form->textField($model,'category_id'); ?>
+			<?php echo $form->listbox($model, 'category_id', $leafs, array( 'size' => 1)  ) ?>
+			<?php echo $form->error($model,'category_id'); ?>
+		</td>
+	</tr>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'desc'); ?>		
-		<?php echo $form->textArea($model,'desc',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'desc'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'content'); ?>
-		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'content'); ?>
-	</div>	
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'category_id'); ?>
-		<?php echo $form->textField($model,'category_id'); ?>
-		<?php echo $form->listbox($model, 'category_id', $leafs, array( 'size' => 1)  ) ?>
-		<?php echo $form->error($model,'category_id'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
-
+	<tr>
+		<th class='leftborder alt'></th>
+		<td>
+			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		</td>
+	</tr>
+	</table>
 <?php $this->endWidget(); ?>
 
-</div><!-- form -->
+</tr><!-- form -->
