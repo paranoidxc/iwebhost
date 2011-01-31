ab81fc4bfb5613552bf50253828d29679edd01c8
Paranoid.xc <paranoid.xc@gmail.com>
2011-01-31 22:58:49 +0800

    user move actionIndex to Parent

--- protected/modules/admin/controllers/UserController.php
+++ protected/modules/admin/controllers/UserController.php
@@ -1,28 +1,7 @@
 <?php
 
-class UserController extends controller
+class UserController extends IController
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
-
-	/**
-	 * @var CActiveRecord the currently loaded data model instance.
-	 */
-	private $_model;
-
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-		);
-	}
-
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
@@ -53,7 +32,7 @@ class UserController extends controller
 	 * Displays a particular model.
 	 */
 	public function actionView()
-	{
+ 	{
 		$this->render('view',array(
 			'model'=>$this->loadModel(),
 		));
@@ -166,38 +145,16 @@ class UserController extends controller
 	 * Lists all models.
 	 */
 	public function actionIndex()
-	{
-	  
-	  //$model=new User('search');
-		//$model->unsetAttributes();  // clear any default values
-		
-		if(isset($_GET['User']))
-			$model->attributes=$_GET['User'];
-		
-		if( isset($_GET['keyword']) ){
-		  $keyword = trim($_GET['keyword']);		  
-		  $list = User::model()->findAll(
-		    array(
-            'condition' => 'username like :keyword OR email like :keyword ',
-            'params'=>array(':keyword'=>"%$keyword%" )            
-        ));      
-		  $this->renderPartial('_index',array('list' => $list),false,true);
-	  }else{
-	    $list = User::model()->findAll();
-  	  $this->render('index',array(
-  			'model' =>  $model,
-  			'list'  =>  $list
-  		));  
+	{	
+	  $criteria=new CDbCriteria;
+		if( isset($_GET['keyword']) || !empty($_GET['keyword']) || strlen($_GET['keyword']) >0  ){
+		  $keyword = trim($_GET['keyword']);			  
+      $criteria->condition  = 'username like :keyword OR email like :keyword ';
+      $criteria->params     = array(':keyword'=>"%$keyword%");      
+      $opt['is_partial']    = true;
+      $opt['criteria'] =  $criteria;
 	  }
-	  
-		
-			
-			/*
-		$dataProvider=new CActiveDataProvider('User');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-		*/
+	  parent::actionIndex($opt);	
 	}
 
 	/**
