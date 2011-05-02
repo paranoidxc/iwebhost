dd735dd9170ccafa7f1a33015ea490110335c5cb
Paranoid.xc <paranoid.xc@gmail.com>
2011-05-03 01:06:13 +0800

    add signin user dropmenu

--- themes/forum/js/script.js
+++ themes/forum/js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){  
+$(document).ready(function(){
+  $('#signin_user_wrap').hover(function(){
+    $($(this).find('ul')).show();
+  },function(){
+    $($(this).find('ul')).hide();
+  });
   $('.timeago').timeago();  
 });
