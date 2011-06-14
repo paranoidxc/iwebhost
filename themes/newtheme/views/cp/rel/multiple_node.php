61e3d52596be6d2e76f1795dd6939dfa57a92b89
Paranoid.xc <paranoid.xc@gmail.com>
2011-06-14 23:03:20 +0800

    multiple node

---
+++ themes/newtheme/views/cp/rel/multiple_node.php
@@ -0,0 +1,12 @@
+<div class='mac_panel_wrap w600p' >
+<?php
+  $nodes  = Category::model()->ileafs( array( 'ident' => 'attachment','include' => true ) );
+?>
+<div style="height: 400px; overflow: auto; background: #FFF;">
+<?php
+  $this->renderPartial('_multiple_node',array( 'nodes' => $nodes,'return_id' => $return_id ) );
+?>
+</div>  	
+<input type="hidden" class="return_id" value="<?php echo $return_id;?>" />
+<?php echo CHtml::submitButton( Yii::t('cp','Submit'), array( 'class' => 'ibtn blue mtl_return_submit' )); ?>
+</div>
