cb97d268e171a461aa3d1eb5adb6207feab3e25b
huangxc <paranoid.xc@gmail.com>
2011-06-30 14:15:31 +0800

    move style

--- themes/newtheme/views/cp/article/move.php
+++ themes/newtheme/views/cp/article/move.php
@@ -1,26 +1,18 @@
-<div class='mac_panel_wrap w600p' >
-<?php
-  $this->beginWidget('application.extensions.Flatmacpanel',array('title'=>Yii::t('cp',"Move to")) )
-?>
+<div class='mac_panel_wrap w600P' >
+  <div class="panel_inner">
+    <div class='wrap_title'>请选择移动到目标类别...<span class='action_normal wrap_cld flR'>关闭</span></div>
 
-<input type="hidden" class="return_panel" value="<?php echo $panel_ident;?>" />
+    <input type="hidden" class="return_panel" value="<?php echo $panel_ident;?>" />
 
+    <div style="height: 400px; overflow: auto; background: #FFF;">
+    <?php $this->renderPartial('//layouts/move_node',array( 'nodes' => $leafs,'return_id' => $return_id ) ); ?>  
+    </div>
 
-  <div style="height: 400px; overflow: auto; background: #FFF;">
-  <?php
-    $this->renderPartial('//layouts/move_node',array( 'nodes' => $leafs,'return_id' => $return_id ) );
-  ?>  
+    <div class="taR h30P lh30P pt5P pr10P dN wrap_footer">    
+      <input type="text" name="category_id"  class="move_category_id hidden_like_span" readonly = true /> - 
+      <input type="text" name="category_name" class="move_category_name hidden_like_span" readonly = true />  
+      <?php echo CHtml::submitButton( Yii::t('cp','Submit'), array( 'class' => 'ibtn batch_move')); ?>
+    </div>  
+
   </div>
-   <div class="taR h30P lh30P pt5P pr10P">    
-    <input type="text" name="category_id"  class="move_category_id hidden_like_span" readonly = true /> - 
-    <input type="text" name="category_name" class="move_category_name hidden_like_span" readonly = true />  
-    <?php echo CHtml::submitButton( Yii::t('cp','Submit'), array( 'class' => 'ibtn blue batch_move')); ?>
-  </div>  
-  
-
-<div class="ajax_overlay" />
-
-<?php
-  $this->endWidget('application.extensions.Flatmacpanel');	 
-?>
 </div>
--- themes/newtheme/views/cp/attachment/move.php
+++ themes/newtheme/views/cp/attachment/move.php
@@ -1,22 +1,17 @@
-<div class='mac_panel_wrap w600p' >
-<?php
-  $this->beginWidget('application.extensions.Flatmacpanel',array('title'=>"Move to") )
-?>
+<div class='mac_panel_wrap w600P' >
+  <div class="panel_inner">
+    <div class='wrap_title'>请选择移动到目标类别...<span class='action_normal wrap_cld flR'>关闭</span></div>
+    <input type="hidden" class="return_panel" value="<?php echo $panel_ident;?>" /> 
+    
+    <div style="height: 400px; overflow: auto; background: #FFF;">
+    <?php $this->renderPartial('//layouts/move_node',array( 'nodes' => $leafs,'return_id' => $return_id ) ); ?>
+    </div>
 
-<input type="hidden" class="return_panel" value="<?php echo $panel_ident;?>" />
-  <?php
-    $this->renderPartial('//layouts/move_node',array( 'nodes' => $leafs,'return_id' => $return_id ) );
-  ?>
-  
-  <div class="taR h30P pr10P">
-    Move To: 
-    <input type="text" size="8" name="category_id"  class="move_category_id" readonly = true /> - 
-    <input type="text" size="40" name="category_name" class="move_category_name" readonly = true />  
-    <?php echo CHtml::submitButton('Ok', array( 'class' => 'ibtn blue batch_move')); ?>
-  </div>  
+    <div class="taR h30P lh30P pt5P pr10P dN wrap_footer">    
+      <input type="text" size="8" name="category_id"  class="move_category_id" readonly = true /> - 
+      <input type="text" size="40" name="category_name" class="move_category_name" readonly = true />  
+      <?php echo CHtml::submitButton('Ok', array( 'class' => 'ibtn batch_move')); ?>
+    </div>  
 
-<div class="ajax_overlay" />
-<?php
-  $this->endWidget('application.extensions.Flatmacpanel');	 
-?>
+  </div>
 </div>
